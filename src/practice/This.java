d74615c98a8209534de97f2943dc496d24ee4cf0
romankhan5503 <romankhan5503@gmail.com>
2025-10-24 17:16:10 +0530

    practice

--- src/practice/This.java
+++ src/practice/This.java
@@ -1,77 +1,83 @@
 package practice;
 
-//🟩 1️⃣ To refer to current class variables
-class Student {
-	String name;
-	int age;
-	
-	Student(String name, int age){
-		this.name = name;  // refers to instance variable
-		this.age = age;
-	}
-	
-	void display() {
-		System.out.println("Name : " + name);
-		System.out.println("Age : " + age);
+class First {
+	int i;
+
+	void m1(int i) {
+		this.i = i;
+		System.out.println(i);
 	}
 }
 
-//🟩 2️⃣ To call current class method
-class Example{
-	void show() {
-		System.out.println("Show method Called");
+class Second {
+
+	void m2() {
+		System.out.println("this is second class");
 	}
-	
-	void display() {
-		this.show();  // calling current class method
+
+	void m3() {
+		m2();
 	}
 }
 
-//🟩 3️⃣⃣ To call another constructor in the same class
-class Book{
-	String title;
-	int price;
-	
-	Book(){
-		this("Unknown", 0);
-		System.out.println("Default constructor");
+class Third {
+
+	Third() {
+		System.out.println("no arg const...");
 	}
 
-	public Book(String title, int price) {
-		this.title = title;
-		this.price = price;
-	}
-	
-	void show() {
-		System.out.println(title + " " + price);
+	Third(int a) {
+		this();
 	}
 }
 
-//🟩 4️⃣ To pass current object as an argument
-class Test{
-	void show(Test t) {
-		System.out.println("Method called using object reference");
+class Fourth {
+	void m4(Fourth f) {
+		System.out.println("in m4");
 	}
-	
-	void display() {
-		show(this);
+
+	void m5() {
+		m4(this);
+	}
+}
+
+class Fifth {
+	Fifth(Sixth ft) {
+		System.out.println("const....");
+	}
+}
+
+class Sixth {
+	void m6() {
+		Fifth fh = new Fifth(this);
+	}
+}
+
+class Seventh{
+	Seventh m7() {
+		return this;
 	}
 }
 
 public class This {
 
 	public static void main(String[] args) {
-		Student s = new Student("Roman", 21);
-		s.display();
-		
-		Example e = new Example();
-		e.display();
-		
-		Book b = new Book();
-		b.show();
 
-		Test obj = new Test();
-        obj.display();
+		First f = new First();
+		f.m1(10);
+
+		Second s = new Second();
+		s.m3();
+
+		Third t = new Third(10);
+
+		Fourth ft = new Fourth();
+		ft.m5();
+
+		Sixth st = new Sixth();
+		st.m6();
+		
+		Seventh sh = new Seventh();
+		sh.m7();
 	}
-
 }
